f885ac91fcd55729f1fb10bc2b19bccdacbfb4d4
Benoit Casault <Benoit.Casault@dfo-mpo.gc.ca>
2020-04-30 11:53:58 -0300

    Added documentation to search_azmpdata.R and ran document()

--- R/search_azmpdata.R
+++ R/search_azmpdata.R
@@ -1,3 +1,13 @@
+#' Search the azmpdata dataset
+#'
+#' Find variables in the azmpdata dataset that match a given keyword.
+#'
+#' @param str string (lowercase only)
+#'
+#' @return character vector
+#' @export
+#'
+#' @examples
 search_azmpdata <- function(str) {
 
   dir("/data/*.Rda")
