92135134396e6b080994870be2b1ee39509da000
Benoit Casault <Benoit.Casault@dfo-mpo.gc.ca>
2020-05-01 12:34:41 -0300

    Added package documentation in /R/azmpdata.R

---
+++ R/azmpdata.R
@@ -0,0 +1,15 @@
+#' azmpdata: A package providing data products derived from data collected as part of
+#' The Atlantic Zone Monitoring Program (AZMP).
+#'
+#' The azmp package provides three categories of data products:
+#' physical, biological and chemical variables.
+#'
+#' @section Physical variables:
+#'
+#' @section Biological variables:
+#'
+#' @section Chemical variables:
+#'
+#' @docType package
+#' @name azmpdata
+NULL
