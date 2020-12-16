5d4adb99af3ed65ebaf104eea01337154bc60ce1
Chantelle Layton <layton.chantelle@gmail.com>
2020-12-16 11:27:01 -0400

    add station 2 data

--- inst/extdata/fixedStationsPhysicalOceanography/fixedStationsPO.R
+++ inst/extdata/fixedStationsPhysicalOceanography/fixedStationsPO.R
@@ -1,17 +1,27 @@
 dataPath <- 'inst/extdata/fixedStationsPhysicalOceanography'
 lookupPath <- 'inst/extdata/lookup'
 
-# 1. read in mission look up tables and merge
+# 1. read in mission look up tables and combine
 lookupfiles <- list.files(lookupPath, pattern = '^mission.*', full.names = TRUE)
 lookup <- lapply(lookupfiles, read.csv)
 missions <- do.call('rbind', lookup)
 
-# 2. read in the data
-datafile <- paste(dataPath, 'prince5.csv', sep = '/')
-d <- read.csv(datafile)
+# 2. read in the data and combine
+datafile <- paste(dataPath, c('prince5.csv', 'station2.csv'), sep = '/')
+d <- lapply(datafile, read.csv)
+d <- do.call('rbind', d)
 
 # 3. match up cruiseNumber and mission_number
-idx <- apply(d, 1, function(k) which(missions[['mission_name']] == k[['cruiseNumber']]))
+# note : if anything goes sideways matching up idx, the structure of idx will change
+#        so it could become a list instead of a vector
+idx <- apply(d, 1, function(k) {ok <- which(missions[['mission_name']] == k[['cruiseNumber']]);
+                                if(length(ok) > 1) {
+                                  ok[1]
+                                  } else if(length(ok) == 0){
+                                    NA
+                                  } else {
+                                    ok
+                                  }})
 
 d <- cbind(d, descriptor = missions[['mission_descriptor']][idx])
 
