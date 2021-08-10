51478d252d78d9795469d349315c6be64002b61a
Jaimie Harbin <jm810136@dal.ca>
2021-08-10 14:01:38 -0300

    adding error messages for plot_availability

--- R/plot_availability.R
+++ R/plot_availability.R
@@ -14,7 +14,69 @@ plot_availability <- function(areaType=NULL,
                               parameters=NULL
 )
 {
-    for (p in seq_along(parameters)) {
+    if (is.null(areaType)) {
+        stop("in plot_availability() :\n provide an areaType of either station, section, or area", call.=FALSE)
+    }
+
+    if (length(areaType) > 1) {
+        stop("in plot_availability() :\n  can only provide one areaType at a time of either station, section, or area", call.=FALSE)
+    }
+
+    if (!areaType %in% c("station", "section", "area")) {
+        stop("in plot_availability() :\n provide an areaType of either station, section, or area", call.=FALSE)
+    }
+
+    if (length(areaType) > 1) {
+        stop("in plot_availability() :\n can only give one areaType at a time", call.=FALSE)
+    }
+
+    unique <- area_indexer(areaTypes=areaType)
+    if (areaType == "station") {
+        s <- unique(unique$station)
+    }
+    if (areaType == "section") {
+        s <- unique(unique$section)
+    }
+
+    if (areaType == "area") {
+        s <- "Gulf of St. Lawrence"
+    }
+
+    if (is.null(areaName)) {
+        # The following is for error messages
+        if (areaType == "station") {
+        stop("in plot_availability() :\n provide areaName of either ", paste(s, collapse=","), call.=FALSE)
+        }
+        if (areaType == "section") {
+        stop("in plot_availability() :\n provide areaName of either ", paste(s, collapse=","), call.=FALSE)
+        }
+
+        if (areaType == "area") {
+        stop("in plot_availability() :\n provide areaName of ", s, call.=FALSE)
+        }
+    }
+
+    if (length(areaName) > 1) {
+        stop("in plot_availability() :\n  can only provide one areaName at a time", call.=FALSE)
+    }
+
+     if (!areaName %in% s) {
+        stop("in plot_availability() :\n", areaName, " is not an areaName within areaType = ", areaType, " try the following ", paste(s, collapse=" "), call.=FALSE)
+    }
+
+
+
+        # The following is for error messages
+        uniquep <- area_indexer(areaTypes=areaType, areanames=areaName, doParameters=T)
+        param <- unique(uniquep$parameter)
+        if (is.null(parameters)) {
+            stop("in plot_availability() :\n when areaname is equal to ", areaName, " must use the following parameters ", paste(param, collapse=","), call.=FALSE)
+        }
+
+        for (p in seq_along(parameters)) {
+        if (!parameters[p] %in% param | is.null(parameters)) {
+        stop("in plot_availability() : ",parameters[p], " not a parameter in ",areaName, " try one of ", param)
+        } else {
         k <- area_indexer(areaTypes=areaType, areanames = areaName, doMonths=T,parameters=parameters[p], doParameters=T)
         dd <- k[which(k$parameter == parameters[p]),]
         df <- data.frame(year=dd[["year"]], month=dd[["month"]])
@@ -32,4 +94,5 @@ plot_availability <- function(areaType=NULL,
         graphics::mtext(side = 2, text = 'Month', line = 2, cex = 4/5)
         graphics::mtext(side=3, text= paste("Frequency table for ", parameters[p], " at the ", areaName, " ", areaType), cex=4/5)
     }
+        }
 }
