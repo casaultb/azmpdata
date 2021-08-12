a84986ea36102f5652c65a9a28878b6f72115249
Jaimie Harbin <jm810136@dal.ca>
2021-08-12 08:50:52 -0300

    fixed merge conflicts

--- R/plot_availability.R
+++ R/plot_availability.R
@@ -8,73 +8,17 @@
 #'  return fields such as "integrated_nitrate_0_50", "integrated_nitrate_50_150", and "nitrate".  If
 #'  you want exact matches only, set \code{fuzzyParameters = F}.
 #' @examples \dontrun{
-#' plot_availability(areaType="section", areaName="p5", parameter="nitrate")
+#' plot_availability(areaType="station", areaName="P5", parameter="nitrate")
 #' }
 #' @author  Jaimie Harbin \email{jaimie.harbin@@dfo-mpo.gc.ca}
 #' @export
-
 plot_availability <- function(areaType=NULL,
                               areaName=NULL,
                               parameters=NULL,
-                              fuzzyParameters = TRUE
+                              fuzzyParameters = FALSE
 )
 {
-    k <- area_indexer(areaTypes=areaType, areanames = areaName, doMonths=T, parameters=parameters,
-                      doParameters=T, fuzzyParameters = fuzzyParameters)
 
-    params <- sort(unique(k$parameter))
-    if (length(params)>3) params = "multiple parameters"
-    areas <- unique(k[!is.na(k$area), "area"])
-    sections <- unique(k[!is.na(k$section), "section"])
-    stations <- unique(k[!is.na(k$station), "station"])
-    if(is.null(areaType)){
-        allAreas <- c(areas, sections, stations)
-        areaDesc <- ""
-    }else if(areaType == "section"){
-        allAreas <- c(sections)
-        areaDesc <- "section(s):"
-    }else if(areaType == "station"){
-        allAreas <- c(stations)
-        areaDesc <- "station(s):"
-    }else if(areaType == "area"){
-        allAreas <- c(areas)
-        areaDesc <- "area(s):"
-    }
-
-    if (length(allAreas)<=3){
-        areaDesc <- paste(areaDesc,paste0(allAreas, collapse = ", "))
-    }else if (length(allAreas)>3 & length(areas)<1 & length(stations)<1) {
-        areaDesc <- "multiple sections"
-    }else if (length(allAreas)>3 & length(areas)<1 & length(sections)<1) {
-        areaDesc <- "multiple stations"
-    }else if (length(allAreas)>3 & length(stations)<1 & length(sections)<1 ){
-        areaDesc <- "multiple areas"
-    }else if (length(allAreas)>3 & length(areas)<1) {
-        areaDesc <- "multiple stations and sections"
-    }else if (length(allAreas)>3 & length(stations)<1) {
-        areaDesc <- "multiple areas and sections"
-    }else if (length(allAreas)>3 & length(sections)<1) {
-        areaDesc <- "multiple areas and stations"
-    }else if (length(allAreas)>3){
-        areaDesc <- "multiple places"
-    }
-
-    freqTable <- with(k, table(year, month))
-
-    cm <- oce::colormap(z = freqTable)
-    x <- as.numeric(rownames(freqTable)) # year
-    y <- as.numeric(colnames(freqTable)) # month
-    oce::imagep(x = x, y = y, z = freqTable,
-                colormap = cm,
-                drawPalette = TRUE)
-    graphics::box()
-    graphics::abline(h = y + 0.5)
-    graphics::abline(v = x + 0.5)
-    graphics::mtext(side = 1, text = 'Year', line = 2, cex = 4/5)
-    graphics::mtext(side = 2, text = 'Month', line = 2, cex = 4/5)
-    graphics::mtext(side=3, text= paste("Frequency table for ", paste0(params, collapse = ","), " at ",areaDesc), cex=4/5)
-    return(freqTable)
-    }
     if (is.null(areaType)) {
         stop("in plot_availability() :\n provide an areaType of either station, section, or area", call.=FALSE)
     }
@@ -103,17 +47,21 @@ plot_availability <- function(areaType=NULL,
         s <- "Gulf of St. Lawrence"
     }
 
+    if (fuzzyParameters == T) {
+    message("Each plot potentially combines multiple parameters matching the value(s) of 'params'.  To avoid this, please specify parameters exactly, and set 'fuzzyParameters' to FALSE")
+    }
+
     if (is.null(areaName)) {
         # The following is for error messages
         if (areaType == "station") {
-        stop("in plot_availability() :\n provide areaName of either ", paste(s, collapse=","), call.=FALSE)
+            stop("in plot_availability() :\n provide areaName of either ", paste(s, collapse=","), call.=FALSE)
         }
         if (areaType == "section") {
-        stop("in plot_availability() :\n provide areaName of either ", paste(s, collapse=","), call.=FALSE)
+            stop("in plot_availability() :\n provide areaName of either ", paste(s, collapse=","), call.=FALSE)
         }
 
         if (areaType == "area") {
-        stop("in plot_availability() :\n provide areaName of ", s, call.=FALSE)
+            stop("in plot_availability() :\n provide areaName of ", s, call.=FALSE)
         }
     }
 
@@ -121,39 +69,76 @@ plot_availability <- function(areaType=NULL,
         stop("in plot_availability() :\n  can only provide one areaName at a time", call.=FALSE)
     }
 
-     if (!areaName %in% s) {
+    if (!areaName %in% s) {
         stop("in plot_availability() :\n", areaName, " is not an areaName within areaType = ", areaType, " try the following ", paste(s, collapse=" "), call.=FALSE)
     }
 
 
-
-        # The following is for error messages
-        uniquep <- area_indexer(areaTypes=areaType, areanames=areaName, doParameters=T)
-        param <- unique(uniquep$parameter)
-        if (is.null(parameters)) {
-            stop("in plot_availability() :\n when areaname is equal to ", areaName, " must use the following parameters ", paste(param, collapse=","), call.=FALSE)
-        }
-
-        for (p in seq_along(parameters)) {
-        if (!parameters[p] %in% param | is.null(parameters)) {
-        stop("in plot_availability() : ",parameters[p], " not a parameter in ",areaName, " try one of ", param)
-        } else {
-        k <- area_indexer(areaTypes=areaType, areanames = areaName, doMonths=T,parameters=parameters[p], doParameters=T)
-        dd <- k[which(k$parameter == parameters[p]),]
-        df <- data.frame(year=dd[["year"]], month=dd[["month"]])
-        freqTable <- with(df, table(year, month))
-        cm <- oce::colormap(z = freqTable)
-        x <- as.numeric(rownames(freqTable)) # year
-        y <- as.numeric(colnames(freqTable)) # month
-        oce::imagep(x = x, y = y, z = freqTable,
-                    colormap = cm,
-                    drawPalette = TRUE)
-        graphics::box()
-        graphics::abline(h = y + 0.5)
-        graphics::abline(v = x + 0.5)
-        graphics::mtext(side = 1, text = 'Year', line = 2, cex = 4/5)
-        graphics::mtext(side = 2, text = 'Month', line = 2, cex = 4/5)
-        graphics::mtext(side=3, text= paste("Frequency table for ", parameters[p], " at the ", areaName, " ", areaType), cex=4/5)
+    # The following is for error messages
+    uniquep <- area_indexer(areaTypes=areaType, areanames=areaName, doParameters=T)
+    param <- unique(uniquep$parameter)
+    if (is.null(parameters)) {
+        stop("in plot_availability() :\n when areaname is equal to ", areaName, " must use the following parameters ", paste(param, collapse=","), call.=FALSE)
     }
+
+
+    for (p in seq_along(parameters)) {
+        if (!parameters[p] %in% param | is.null(parameters)) {
+            stop("in plot_availability() : ",parameters[p], " not a parameter in ",areaName, " try one of ", paste(param, collapse=" ")) } else {
+                k <- area_indexer(areaTypes=areaType, areanames = areaName, doMonths=T, parameters=parameters[p],
+                                  doParameters=T, fuzzyParameters = fuzzyParameters)
+
+                params <- sort(unique(k$parameter))
+                if (length(params)>3) params = "multiple parameters"
+                areas <- unique(k[!is.na(k$area), "area"])
+                sections <- unique(k[!is.na(k$section), "section"])
+                stations <- unique(k[!is.na(k$station), "station"])
+                if(is.null(areaType)){
+                    allAreas <- c(areas, sections, stations)
+                    areaDesc <- ""
+                }else if(areaType == "section"){
+                    allAreas <- c(sections)
+                    areaDesc <- "section(s):"
+                }else if(areaType == "station"){
+                    allAreas <- c(stations)
+                    areaDesc <- "station(s):"
+                }else if(areaType == "area"){
+                    allAreas <- c(areas)
+                    areaDesc <- "area(s):"
+                }
+
+                if (length(allAreas)<=3){
+                    areaDesc <- paste(areaDesc,paste0(allAreas, collapse = ", "))
+                }else if (length(allAreas)>3 & length(areas)<1 & length(stations)<1) {
+                    areaDesc <- "multiple sections"
+                }else if (length(allAreas)>3 & length(areas)<1 & length(sections)<1) {
+                    areaDesc <- "multiple stations"
+                }else if (length(allAreas)>3 & length(stations)<1 & length(sections)<1 ){
+                    areaDesc <- "multiple areas"
+                }else if (length(allAreas)>3 & length(areas)<1) {
+                    areaDesc <- "multiple stations and sections"
+                }else if (length(allAreas)>3 & length(stations)<1) {
+                    areaDesc <- "multiple areas and sections"
+                }else if (length(allAreas)>3 & length(sections)<1) {
+                    areaDesc <- "multiple areas and stations"
+                }else if (length(allAreas)>3){
+                    areaDesc <- "multiple places"
+                }
+
+                freqTable <- with(k, table(year, month))
+
+                cm <- oce::colormap(z = freqTable)
+                x <- as.numeric(rownames(freqTable)) # year
+                y <- as.numeric(colnames(freqTable)) # month
+                oce::imagep(x = x, y = y, z = freqTable,
+                            colormap = cm,
+                            drawPalette = TRUE)
+                graphics::box()
+                graphics::abline(h = y + 0.5)
+                graphics::abline(v = x + 0.5)
+                graphics::mtext(side = 1, text = 'Year', line = 2, cex = 4/5)
+                graphics::mtext(side = 2, text = 'Month', line = 2, cex = 4/5)
+                graphics::mtext(side=3, text= paste("Frequency table for ", paste0(params, collapse = ","), " at ",areaDesc), cex=4/5)
         }
-}
+
+}}
