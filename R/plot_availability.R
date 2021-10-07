2ab1441d63284d9cea1046168f5a1403932bdb06
McMahon <Mike.McMahon@dfo-mpo.gc.ca>
2021-10-07 09:57:24 -0300

    fixes error message info

--- R/plot_availability.R
+++ R/plot_availability.R
@@ -25,7 +25,6 @@ plot_availability <- function(areaType=NULL,
     FailName <- FALSE
     FailParam <- FALSE
 
-    # Note: This is for area
     if (length(areaName) > 1) stop("in plot_availability() :\n  can only provide one areaName at a time", call.=FALSE)
     if (is.null(areaType)) stop("in plot_availability() :\n provide an areaType of either station, section, or area", call.=FALSE)
     if (length(areaType) > 1) stop("in plot_availability() :\n  can only provide one areaType at a time of either station, section, or area", call.=FALSE)
@@ -93,7 +92,7 @@ plot_availability <- function(areaType=NULL,
     }
 
     if (FailName & FailParam) {
-        message("For ", areaType,"s, valid places for your selected parameters (i.e. ",paste(availParams, collapse=", "),") include:")
+        message("For ", areaType,"s, valid places for your selected parameters (i.e. ",paste(parameters, collapse=", "),") include:")
         message(paste(availAreas, collapse=", "))
         message("For ", areaType,"s, (specifically, ",areaName,"), valid parameters include:")
         message(paste(availParams, collapse=", "))
@@ -123,7 +122,7 @@ remainingDataAgg <- aggregate(
 
         freqTable <- stats::reshape(thisParamDataAgg, idvar=c('year','xx','parameter'), timevar='month',direction='wide')
         attr(freqTable, "reshapeWide") <- NULL
-        freqTable = freqTable[with(freqTable, order(year)), ]
+        freqTable <- freqTable[with(freqTable, order(year)), ]
         freqTable$xx <- freqTable$parameter <- NULL
         colnames(freqTable) <- sub("cnt\\.", "", colnames(freqTable))
         freqTable[is.na(freqTable)] <- 0
